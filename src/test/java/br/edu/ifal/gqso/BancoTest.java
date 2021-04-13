22bf078d99380eac9b8e073b6bec3a0c0450943b
gaabrieljesuss <gjs1@aluno.ifal.edu.br>
2021-04-13 18:25:42 +0000

    Criação do método saldoTest na classe BancoTest

--- src/test/java/br/edu/ifal/gqso/BancoTest.java
+++ src/test/java/br/edu/ifal/gqso/BancoTest.java
@@ -1,7 +1,19 @@
 package br.edu.ifal.gqso;
 
-
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class BancoTest {
-    
+    private Banco banco;
+
+
+    @BeforeEach
+    public void setUp(){
+        this.banco = new Banco();
+    }
+
+    @Test
+    public void saldoTest(){
+        
+    }
 }
