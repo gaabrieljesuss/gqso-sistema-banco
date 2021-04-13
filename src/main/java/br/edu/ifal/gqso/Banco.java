a67a2f8772bd132e6d3830675f8ee0af19df366d
gaabrieljesuss <gjs1@aluno.ifal.edu.br>
2021-04-13 19:39:50 +0000

    Criando a exception SaldoInsuficiente

--- src/main/java/br/edu/ifal/gqso/Banco.java
+++ src/main/java/br/edu/ifal/gqso/Banco.java
@@ -3,6 +3,11 @@ package br.edu.ifal.gqso;
 public class Banco {
     private double saldo;
     
+    public class SaldoInsuficiente extends Exception{
+        private static final long serialVersionUID = 1L;
+    }
+
+
     public double saldo(){
         return this.saldo;
     }
@@ -11,7 +16,8 @@ public class Banco {
         this.saldo = saldo;
     }
 
-    public double saque(double valor){
+    public double saque(double valor) {
+        
         setSaldo(saldo() - valor);
         return saldo();
     }
