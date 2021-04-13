d83816b08738c0d6ee1850ba2c84cfc7556a82d1
gaabrieljesuss <gjs1@aluno.ifal.edu.br>
2021-04-13 18:34:45 +0000

    Teste inicial do método saldo falhando

--- src/test/java/br/edu/ifal/gqso/BancoTest.java
+++ src/test/java/br/edu/ifal/gqso/BancoTest.java
@@ -1,5 +1,7 @@
 package br.edu.ifal.gqso;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +12,11 @@ public class BancoTest {
     @BeforeEach
     public void setUp(){
         this.banco = new Banco();
+        banco.setSaldo(200);
     }
 
     @Test
     public void saldoTest(){
-        
+        assertEquals(200, banco.saldo());
     }
 }
